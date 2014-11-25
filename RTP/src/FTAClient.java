3ebad638de0813be5994b7491adbf462ab4a02f5
kogilbert <kogilbert100@gmail.com>
2014-11-25 11:30:59 -0500

    altered client input to match assignment

--- RTP/src/FTAClient.java
+++ RTP/src/FTAClient.java
@@ -17,52 +17,64 @@ public class FTAClient {
 	//576 - 20 -8 -16 = 531
 	
 	public static void main(String[] args) throws IOException {
-		
-		int emuPort=0;
-		int clientPort=0;
-		int desPort=0;
-		InetAddress serverAddress=null;
 		RTP rtpProtocol=null;
 		
-		System.out.println("Welcome to the client, type 'connect(Emulator IP, Emulator Port Number, Client Port Number)");
+		// lllegalArgumentException("Parameter(s)' <Server> <Word> [<Port>]");
+		if ((args.length < 3) || (args.length > 4)) {
+			throw new IOException("Invalid Argument");
+		}
+		
+		//client port
+		int clientPort = Integer.parseInt(args[0]);
+		// Server IP address
+		InetAddress serverAddress = InetAddress.getByName(args[1]);
+		// Emu port num
+		int emuPort = Integer.parseInt(args[2]);
+		//Dest port
+		int desPort =clientPort+1;
+
+
+		
+
 		Scanner sc = new Scanner(System.in);
 		boolean connected= false;
-		while(connected){
-			String input = sc.nextLine();
-			System.out.println("you inputed " + input);
-			if(input.contains("connect")){
+		
+		while(true){
+			while(!connected){
+				System.out.println("Welcome to the client, type 'connect");
+				String input = sc.nextLine();
+				System.out.println("you inputed " + input);
+				if(input.contains("connect")){
+					rtpProtocol = new RTP(serverAddress, emuPort, clientPort, desPort);
+					rtpProtocol.connect();
+					connected = true;
+				}
+			}
+	
+			while(connected){
+				System.out.println("type 'post filename, get filename, Window W, or disconnect");
+				String input = sc.nextLine();
+				System.out.println("you inputed " + input);
 				
-				String emIP = input.substring(input.indexOf('(')+1, input.indexOf(','));
-				System.out.println(emIP);
-				serverAddress = InetAddress.getByName(emIP);
-				input = input.substring(input.indexOf(',')+2);
-				emuPort = Integer.parseInt(input.substring(0, input.indexOf(',')));
-				System.out.println(emuPort);
-				input = input.substring(input.indexOf(',')+2);
-				clientPort = Integer.parseInt(input.substring(0, input.indexOf(')')));
-				System.out.println(clientPort);
-				desPort = clientPort+1;
-				rtpProtocol = new RTP(serverAddress, emuPort, clientPort, desPort);
-				rtpProtocol.connect();
+				if(input.contains("post")){
+					String filename = input.substring(input.indexOf('t')+2);
+					FileInputStream fileIn = new FileInputStream(System.getProperty("user.dir") + "/" + filename);
+					rtpProtocol.sendFile(fileIn);	
+				} else if(input.contains("get")){
+					String filename = input.substring(input.indexOf('t')+2);
+					//get file stuff
+	
+				}else if(input.contains("window")){
+					//change window
+					
+				}else if(input.contains("disconnect")){
+					//rtpProtocol.close();
+					connected = false;
 
-				connected = true;
+				}
 			}
 		}
-
-		while(connected)
-		System.out.println("type 'postfile(filename), getfile(filename), or disconnect");
-		String input = sc.nextLine();
-		System.out.println("you inputed " + input);
 		
-		if(input.contains("postfile")){
-			String filename = input.substring(input.indexOf('(')+1, input.indexOf(')'));
-			FileInputStream fileIn = new FileInputStream(System.getProperty("user.dir") + "/" + filename);
-			rtpProtocol.sendFile(fileIn);	
-		} else if(input.contains("getfile")){
-			//get file stuff
-		}else if(input.contains("disconnect")){
-			rtpProtocol.close();
-		}
 		
 		
 		
@@ -77,15 +89,11 @@ public class FTAClient {
 		}
 */
 		/*
-		int clientPort = Integer.parseInt(args[3]);
 
-		// Server IP address
-		InetAddress serverAddress = InetAddress.getByName(args[0]);
 
-		// Server port num
-		int emuPort = Integer.parseInt(args[1]);
-		
-		int desPort =clientPort+1;
+
+
+
 		
 		RTP rtpProtocol = new RTP(serverAddress, emuPort, clientPort, desPort);
 		
