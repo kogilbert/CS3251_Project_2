3de12630a8a7ca4e5649ec2df0d606b49d2bb45a
kogilbert <kogilbert100@gmail.com>
2014-11-25 00:42:53 -0500

    added user input to client

--- RTP/src/FTAClient.java
+++ RTP/src/FTAClient.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 /*
  * function Connect (Port num, Emulator IP, Emulator Port num) 
@@ -16,21 +17,66 @@ public class FTAClient {
 	//576 - 20 -8 -16 = 531
 	
 	public static void main(String[] args) throws IOException {
-		System.out.println("Welcome to the client, type 'connect(Port Number, Emulator IP, Emulator Port Number)");
-		//Scanner sc = new Scanner(System.in);
-		//String input = sc.nextLine();
-		//System.out.println("you inputed " + input);
+		
+		int emuPort=0;
+		int clientPort=0;
+		int desPort=0;
+		InetAddress serverAddress=null;
+		RTP rtpProtocol=null;
+		
+		System.out.println("Welcome to the client, type 'connect(Emulator IP, Emulator Port Number, Client Port Number)");
+		Scanner sc = new Scanner(System.in);
+		boolean notConnected= true;
+		while(notConnected){
+			String input = sc.nextLine();
+			System.out.println("you inputed " + input);
+			if(input.contains("connect")){
+				
+				String emIP = input.substring(input.indexOf('(')+1, input.indexOf(','));
+				System.out.println(emIP);
+				serverAddress = InetAddress.getByName(emIP);
+				input = input.substring(input.indexOf(',')+2);
+				emuPort = Integer.parseInt(input.substring(0, input.indexOf(',')));
+				System.out.println(emuPort);
+				input = input.substring(input.indexOf(',')+2);
+				clientPort = Integer.parseInt(input.substring(0, input.indexOf(')')));
+				System.out.println(clientPort);
+				desPort = clientPort+1;
+				rtpProtocol = new RTP(serverAddress, emuPort, clientPort, desPort);
+				rtpProtocol.connect();
+
+				notConnected = false;
+			}
+		}
+
+
+		System.out.println("type 'postfile(filename) or getfile(filename)");
+		String input = sc.nextLine();
+		System.out.println("you inputed " + input);
+		
+		if(input.contains("postfile")){
+			String filename = input.substring(input.indexOf('(')+1, input.indexOf(')'));
+			FileInputStream fileIn = new FileInputStream(System.getProperty("user.dir") + "/" + filename);
+			rtpProtocol.sendFile(fileIn);	
+		} else if(input.contains("getfile")){
+			//get file stuff
+		}else if(input.contains("disconnect")){
+			//rtpProtocol.close();
+		}
+		
+		
 		
 		/**
 		 * Set up user input parameters Port Number, Emulator IP, Emulator Port Number------------------------------
 		 */
-		
+		/*
 		// Test for correct # of args throw new
 		// lllegalArgumentException("Parameter(s)' <Server> <Word> [<Port>]");
 		if ((args.length < 3) || (args.length > 4)) {
 			throw new IOException("Invalid Argument");
 		}
-
+*/
+		/*
 		int clientPort = Integer.parseInt(args[3]);
 
 		// Server IP address
@@ -47,7 +93,7 @@ public class FTAClient {
 		/**
 		 * Start sending and receiving data------------------------------------------------------------------------
 		 */
-		
+		/*
 		rtpProtocol.connect();
 		
 		String filename = args[2];
@@ -57,7 +103,7 @@ public class FTAClient {
 		rtpProtocol.sendFile(fileIn);
 		
 		//rtpProtocol.close();
-
+*/
 	}
 
 }
